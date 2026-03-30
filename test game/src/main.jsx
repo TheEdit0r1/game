b978502afa8c5d714b30c5a3e54b8d55b7e235b7
TheEdit0r1 <83130976+TheEdit0r1@users.noreply.github.com>
2026-03-30 21:36:24 +0100

    Switch from BrowserRouter to HashRouter

--- test game/src/main.jsx
+++ test game/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'  // <-- change here
 import App from './App'
 import { GameProvider } from './context/GameContext'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GameProvider>
         <App />
       </GameProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
